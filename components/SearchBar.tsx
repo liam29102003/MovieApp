3514e6bb9f7a5c09071d737efd9f6325c943be94
Liam <lkhant616@gmail.com>
2025-04-15 11:24:13 +0630

    Latest movies added to home page

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,17 +1,34 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native'
-import React from 'react'
-import { Image } from 'react-native';
-import { icons } from '../constants/icons.ts';
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React from "react";
+import { Image } from "react-native";
+import { icons } from "../constants/icons.ts";
 
-const SearchBar = () => {
-  return (
-    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
-      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff"/>
-      <TextInput onPress={() => {}} placeholder="Search" value="" onChangeText={() => {}} placeholderTextColor="#a8b5db" className="flex-1 ml-2 text-white" />
-    </View>
-  )
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
 }
 
-export default SearchBar
+const SearchBar = ({ placeholder, onPress }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default SearchBar;
+
+const styles = StyleSheet.create({});
