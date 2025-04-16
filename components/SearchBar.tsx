4f50d280f1b28b18dc770c6e548b15b97363302f
Liam <lkhant616@gmail.com>
2025-04-16 18:30:54 +0630

    Search Page developed

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,11 +6,13 @@ import { icons } from "../constants/icons.ts";
 interface Props {
   placeholder: string;
   onPress?: () => void;
+  value: string;
+  onChangeText?: (text: string)=>void;
 }
 
-const SearchBar = ({ placeholder, onPress }: Props) => {
+const SearchBar = ({ placeholder, onPress, value, onChangeText }: Props) => {
   return (
-    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-3">
       <Image
         source={icons.search}
         className="size-5"
@@ -20,8 +22,8 @@ const SearchBar = ({ placeholder, onPress }: Props) => {
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor="#a8b5db"
         className="flex-1 ml-2 text-white"
       />
